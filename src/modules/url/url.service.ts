d2078d8727cfa2411d653bd5175e1b8659dc96da
Konrad Pazgan <hello@konradpazgan.pl>
2023-12-26 21:04:02 +0100

    Add ability to get filtered and paginated results

--- src/modules/url/url.service.ts
+++ src/modules/url/url.service.ts
@@ -4,6 +4,7 @@ import { UpdateUrlDto } from './dto/update-url.dto';
 import { UidService } from '../../services/uid/uid.service';
 import { DatabaseService } from '../../database/database.service';
 import { ConfigService } from '@nestjs/config';
+import { FilterUrlsDto } from './dto/filter-urls.dto';
 
 @Injectable()
 export class UrlService {
@@ -12,9 +13,7 @@ export class UrlService {
     private readonly uidService: UidService,
     private readonly databaseService: DatabaseService,
     private readonly configService: ConfigService,
-  ) {}
-
-  onModuleInit() {
+  ) {
     this.host = this.configService.getOrThrow(`host`);
   }
 
@@ -29,8 +28,37 @@ export class UrlService {
     return url;
   }
 
-  findAll() {
-    return `This action returns all url`;
+  async findAll({ page = 1, limit = 1, filter }: FilterUrlsDto) {
+    const skip = (page - 1) * limit;
+    const whereClause = filter
+      ? {
+          OR: [
+            { title: { contains: filter } },
+            { description: { contains: filter } },
+            { redirect: { contains: filter } },
+            { url: { contains: filter } },
+          ],
+        }
+      : {};
+    const data = await this.databaseService.url.findMany({
+      where: whereClause,
+      skip,
+      take: limit,
+    });
+    const totalCount = await this.databaseService.url.count({
+      where: whereClause,
+    });
+
+    const meta = {
+      totalCount,
+      currentPage: page,
+      perPage: limit,
+      totalPages: Math.ceil(totalCount / limit),
+      hasNextPage: skip + limit < totalCount,
+      hasPrevPage: skip > 0 && page > 1,
+    };
+
+    return { data, meta };
   }
 
   async findOne(uid: string) {
