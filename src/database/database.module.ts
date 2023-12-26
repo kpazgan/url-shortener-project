f88ed21d762966855263ac61dae1bab6a097b6c6
Konrad Pazgan <hello@konradpazgan.pl>
2023-12-26 15:25:53 +0100

    Add possibility to create Url

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -4,6 +4,6 @@ import { DatabaseService } from './database.service';
 @Global()
 @Module({
   providers: [DatabaseService],
-  exports: [DatabaseModule],
+  exports: [DatabaseService],
 })
 export class DatabaseModule {}
